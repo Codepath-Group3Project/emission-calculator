f477eea663e1e866f32b2d96fbc0971741b09917
Lucas Benda <54371703+lukebenda@users.noreply.github.com>
2022-10-26 11:00:53 -0500

    Added app logo, created launch screen, started login & home view controller files

---
+++ emissi0n/emissi0n/SignInViewController.swift
@@ -0,0 +1,31 @@
+// emissi0n
+// SignInViewController.swift
+// Created by Lucas Benda on  10/26/22 AT 10:49 AM.
+// 
+// Swift version  5.0
+// Running on macOS 12.5
+
+
+import UIKit
+import Parse
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
