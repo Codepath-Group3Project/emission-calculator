b098837c1525bb023c180104bd2385ce90657a2a
GMeem <59401001+GMeem@users.noreply.github.com>
2022-11-02 20:30:17 -0400

    progress bar

---
+++ emissi0n/emissi0n/ProgressBarViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ProgressBarViewController.swift
+//  emissi0n
+//
+//  Created by Gulshan Meem on 11/2/22.
+//
+
+import UIKit
+
+class ProgressBarViewController: UIViewController {
+
+  
+    let ProgressLayer = CAShapeLayer()
+    let trackLayer = CAShapeLayer()
+    var limit = 0
+    var total = 0
+        
+    let label: UILabel = {
+            let label = UILabel()
+            label.textAlignment = .center
+            label.numberOfLines = 0
+            label.font = .systemFont(ofSize: 36)
+            return label
+        }()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+        
+            limit = 400
+            total = 300
+            
+            createCircularProgressBar(limit: CGFloat(limit),total: CGFloat(total))
+        }
+        
+        func createCircularProgressBar(limit:CGFloat, total:CGFloat){
+            let center =  view.center
+            label.text = "\(Int(total)) \n out of \n \(Int(limit)) "
+            label.sizeToFit()
+            view.addSubview(label)
+            label.center = center
+            
+            // create my track layer
+            
+            
+            let circularPath = UIBezierPath(arcCenter: center, radius: 120, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+            trackLayer.path = circularPath.cgPath
+            
+            trackLayer.strokeColor = UIColor.lightGray.cgColor
+            trackLayer.lineWidth = 12
+            trackLayer.fillColor = UIColor.clear.cgColor
+            trackLayer.lineCap = CAShapeLayerLineCap.round
+            view.layer.addSublayer(trackLayer)
+            
+            ProgressLayer.path = circularPath.cgPath
+            
+            ProgressLayer.strokeColor = UIColor.green.cgColor
+            ProgressLayer.lineWidth = 12
+            ProgressLayer.fillColor = UIColor.clear.cgColor
+            ProgressLayer.lineCap = CAShapeLayerLineCap.round
+            
+            ProgressLayer.strokeEnd = 0.9 - 0.13
+            
+            if (ProgressLayer.strokeEnd > 0.7 ){
+                ProgressLayer.strokeColor = UIColor.red.cgColor
+            } else if (ProgressLayer.strokeEnd > 0.5){
+                ProgressLayer.strokeColor = UIColor.orange.cgColor
+            }else if (ProgressLayer.strokeEnd > 0.4){
+                ProgressLayer.strokeColor = UIColor.yellow.cgColor
+            }
+            
+            view.layer.addSublayer(ProgressLayer)
+            
+            //view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        
+            
+        }
+        
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
